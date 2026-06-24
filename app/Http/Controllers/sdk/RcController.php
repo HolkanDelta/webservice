761d8063d057b23a266ce033cb25e56c9ae916cf
Dissaor999 <dan.v999@gmail.com>
2026-06-23 19:00:31 -0600

    Logística tres Guerreras generic walmart

--- app/Http/Controllers/sdk/RcController.php
+++ app/Http/Controllers/sdk/RcController.php
@@ -15,12 +15,12 @@ class RcController extends Controller
     public function RCServiceLogin(RecursoConfiable $gpsService, $client): JsonResponse
     {
         $resultado = $gpsService->callMethod('GetUserToken', [
-                    'userId' => $client->user_name,
-                    'password' => $client->user_pass,
-                ]);
+            'userId' => $client->user_name,
+            'password' => $client->user_pass,
+        ]);
         $token = $resultado->GetUserTokenResult->token;
         //dd($token);
-        
+
         $client->token = $token;
         $client->save();
         return response()->json($resultado);
@@ -33,69 +33,70 @@ class RcController extends Controller
         $units = json_decode($unitslist->getContent());
         $unitsArray = [];
         $payload_data = [];
-       
+
         foreach ($units->data->units as $unit) {
             $unitsArray[] = $unit->unit_id;
         }
-        
-        foreach ($unitsArray as $unit_id) {            
+
+        foreach ($unitsArray as $unit_id) {
             $unitsind = $sdkMapon->units_id($client->apikey, $unit_id);
             $unitsind = json_decode($unitsind->getContent());
             foreach ($unitsind->data->units as $unit) {
                 $driving = $unit->state->name;
                 $imei = (string) ($unit->device?->imei ?? $unit->vin);
-                
-                //if ($driving == "driving") {                    
-                    //declara variable de customer
-                    $clientes66319 = [
-                        "Ramiro Enrique Vargas Romero",
-                        "Ernesto Soto Molina - Recurso Confiable WALMART",
-                        "Ernesto Soto Molina",
 
-                    ];
-                    $cname = $client->name;
-                    if (in_array($client->name, $clientes66319)) {
-                        $cname = '66319';
-                        //dd($cname);
-                    }
-                    $company = [
-                        'id' => $client->company_id,
-                        'name' => $cname,
-                    ];
-                    //construye payload
-                    $payload_data["Event"][] = [
-                                    'altitude' => '0',
-                                    'asset' => $unit->number ? $unit->number : $unit->label,
-                                    'battery' =>  '0',
-                                    'code' => '0',
-                                    'course' => $unit->direction ? (string) $unit->direction : '0',
-                                    'customer' => $company,
-                                    'date' => $unit->last_update ? (string) $unit->last_update : '0',
-                                    'direction' => '0',
-                                    'humidity' => '0.00',
-                                    'ignition' => true,
-                                    'latitude' => $unit->lat ?  $unit->lat : '0',
-                                    'longitude' => $unit->lng ?  $unit->lng : '0',
-                                    'odometer' => $unit->mileage ? (string) $unit->mileage : '0',
-                                    'serialNumber' => $imei,
-                                    'shipment' => '0',                          
-                                    'speed' => $unit->speed ? (string) $unit->speed : '0',
-                                    'temperature' => '0.00',
-                                    'vehicleType' => $unit->type ? $unit->type : '0',
-                                    'vehicleBrand' => '0',
-                                    'vehicleModel' => '0',                            
-                    ];                    
+                //if ($driving == "driving") {                    
+                //declara variable de customer
+                $clientes66319 = [
+                    "Ramiro Enrique Vargas Romero",
+                    "Ernesto Soto Molina - Recurso Confiable WALMART",
+                    "Ernesto Soto Molina",
+                    "Logística tres Guerreras"
+
+                ];
+                $cname = $client->name;
+                if (in_array($client->name, $clientes66319)) {
+                    $cname = '66319';
+                    //dd($cname);
+                }
+                $company = [
+                    'id' => $client->company_id,
+                    'name' => $cname,
+                ];
+                //construye payload
+                $payload_data["Event"][] = [
+                    'altitude' => '0',
+                    'asset' => $unit->number ? $unit->number : $unit->label,
+                    'battery' =>  '0',
+                    'code' => '0',
+                    'course' => $unit->direction ? (string) $unit->direction : '0',
+                    'customer' => $company,
+                    'date' => $unit->last_update ? (string) $unit->last_update : '0',
+                    'direction' => '0',
+                    'humidity' => '0.00',
+                    'ignition' => true,
+                    'latitude' => $unit->lat ?  $unit->lat : '0',
+                    'longitude' => $unit->lng ?  $unit->lng : '0',
+                    'odometer' => $unit->mileage ? (string) $unit->mileage : '0',
+                    'serialNumber' => $imei,
+                    'shipment' => '0',
+                    'speed' => $unit->speed ? (string) $unit->speed : '0',
+                    'temperature' => '0.00',
+                    'vehicleType' => $unit->type ? $unit->type : '0',
+                    'vehicleBrand' => '0',
+                    'vehicleModel' => '0',
+                ];
                 //}                
-            } 
+            }
         }
         //dd($payload_data);
         $date = date('Y-m-d H:i:s');
         $filename = 'recurso_confiable_' . $date . '.log';
         $payload = json_encode($payload_data);
         $resultado = $gpsService->callMethod('GPSAssetTracking', [
-                      'token' => $client->token,
-                      'events' => $payload_data,
-                  ]);
+            'token' => $client->token,
+            'events' => $payload_data,
+        ]);
         $rawResponse = $gpsService->getLastResponse();
         $log_resultado_arr = [
             'payload' => $payload_data,
